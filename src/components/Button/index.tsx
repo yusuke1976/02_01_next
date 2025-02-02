15447b20eb8b89ce7f1a9dce05b724a1dd849a26
yusuke1976 <yusuke_1483@yahoo.co.jp>
2025-02-02 13:49:20 +0900

    修正

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,21 @@
 // rafceと入力後にタブキーを押しましょう🤗
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
-
 // styled-componentsの使い方
-const Test = styled.div<{ textColor:string; bg:string }>`
+const Test = styled.div<{ textColor: string; bg: string }>`
   color: ${(props) => props.textColor};
   border: 1px solid black;
   padding: 20px;
-  background: ${(props) => props.bg}; 
-`
+  background: ${(props) => props.bg};
+`;
 
+const Button = ({ bg = "white", textColor = "black" }) => {
+  return (
+    <Test textColor={textColor} bg={bg}>
+      Button
+    </Test>
+  );
+};
 
-const Button = ({bg,textColor}) => {
-  return <Test textColor={textColor} bg={bg}>Button</Test>;
-}
-
-export default Button
+export default Button;
